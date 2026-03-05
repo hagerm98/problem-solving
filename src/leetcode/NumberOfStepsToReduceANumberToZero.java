8318bde0059dc01e23d9b392b65c06ba4c5f0303
Hager Khamis <hagerm98@hotmail.com>
2026-03-05 10:28:44 +0000

    1342. Number of Steps to Reduce a Number to Zero Solution

---
+++ src/leetcode/NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+public class NumberOfStepsToReduceANumberToZero {
+    public int numberOfSteps(int num) {
+
+        if (num == 0) {
+            return 0;
+        }
+
+        int steps = 0;
+
+        while (num > 0) {
+
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num--;
+            }
+
+            steps++;
+        }
+
+        return steps;
+    }
+}
