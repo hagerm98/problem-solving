cecd2bc502379f585bc5ed7381db6ec013cfeb90
Hager Khamis <hagerm98@hotmail.com>
2026-03-05 18:09:46 +0000

    1. Two Sum Solution

---
+++ src/leetcode/TwoSum.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class TwoSum {
+
+    public static int[] twoSumBruteforce(int[] nums, int target) {
+
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+
+        throw new RuntimeException("No pair of elements in nums sum to target: " + target);
+    }
+
+    public static int[] twoSumMap(int[] nums, int target) {
+
+        int[] result = new int[2];
+
+        Map<Integer, Integer> numsIndices = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (numsIndices.containsKey(target - nums[i])) {
+                result[0] = numsIndices.get(target - nums[i]);
+                result[1] = i;
+                return result;
+            } else {
+                numsIndices.put(nums[i], i);
+            }
+        }
+
+        throw new RuntimeException("No pair of elements in nums sum to target: " + target);
+
+    }
+
+}
